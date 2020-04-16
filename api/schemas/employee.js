d1b46aa0cd541f41cd71f9e8fe107801ffa13757
balani98 <balanideepanshu92@gmail.com>
2020-04-16 16:15:09 +0530

    schemas designed and added in boiler plate

---
+++ api/schemas/employee.js
@@ -0,0 +1,43 @@
+const mongoose=require("mongoose");
+var ObjectId=mongoose.Schema.Types.ObjectId;
+module.exports={
+    name:{
+      type:String,
+      required:true,
+      minlength:2,
+      maxlength:100
+    },
+    designation:{
+       type:String,
+       required:true,
+       enum:["intern","consultant1","consultant2","associate1","associate2","manager1","manager2"],
+       default:"consultant1"
+    },
+    role:{
+        type:String,
+        required:true,
+        enum:["admin","hr","interviewer"]
+    },
+    employeeId:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        minlength:5,
+        maxlength:100,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    profileImageURL:{
+        type:String,
+        default:null
+    }
+    
+}
--- api/schemas/index.js
+++ api/schemas/index.js
@@ -1,3 +1,15 @@
-module.exports = {
-    
+const jobDescription=require("./jobDescription");
+const candidate=require("./candidate");
+const employee=require("./employee");
+const criteria=require("./criteria");
+const employee=require("./employee");
+const interview=require("./interview");
+const round=require("./round");
+module.exports={
+  employee:employee,
+  candidate:candidate,
+  criteria:criteria,
+  jobDescription:jobDescription,
+  interview:interview,
+  round:round
 }
