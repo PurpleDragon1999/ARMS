d06ee64157b01a419b56f96b737dbdb6c619b7ad
Shubham Sharma <ss320163@gmail.com>
2020-04-21 02:07:10 +0530

    Navbar for Boilerplate is partially done

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "navbar", component: NavBarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
