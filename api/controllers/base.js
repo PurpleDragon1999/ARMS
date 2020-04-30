b91005ba3fc0b4976c6b47904e531440a5685c56
nangchetnamaw <nangchetnamaw@gmail.com>
2020-04-30 18:32:31 +0530

    uploaded proper file and removed unnecessary code

--- api/controllers/base.js
+++ api/controllers/base.js
@@ -47,33 +47,48 @@ class Base {
         }
     }
 
-   async modify(req, res){
-        await this.model.modify(req.params.id, req.body);
-    }
-  
-  async index(req, res){
-      try{
-          const data = await this.model.index();
-          return res.send({
-              success: true,
-              payload: {
-                  data,
-                  message: 'Retrieved Successfully'
-              }
-          });
-      }catch(e){
-          res.status(500).send({
-              success: false,
-              payload: {
-                  message: e.message
-              }
-          })
-      }
-  }
 
-    async remove(req, res){
-        await this.model.remove(req.params.id);
+
+    async modify(req, res) {
+        try {
+          const data = await this.model.modify(req.params.id, req.body);
+          return res.send({
+            success: true,
+            payload: {
+              data,
+              message: "Modified Successfully",
+            },
+          });
+        } catch (e) {
+          res.status(500).send({
+            success: false,
+            payload: {
+              message: e.message,
+            }
+          });
+        }
+      }
+  
+    async index(req, res){
+        try{
+            const data = await this.model.index();
+            return res.send({
+                success: true,
+                payload: {
+                    data,
+                    message: 'Retrieved Successfully'
+                }
+            });
+        }catch(e){
+            res.status(500).send({
+                success: false,
+                payload: {
+                    message: e.message
+                }
+            })
+        }
     }
+
 
     async create(req,res){
         try{    
@@ -292,37 +307,6 @@ class Base {
     }
    
     
-    // async getAll(req, res) {
-    //     try {
-    //     const recordList = await this.model.getAll();
-    //     const page = parseInt(req.query.page) || 1;
-    //     const pageSize = 10;
-    //     const pager = await pagination.paginate(
-    //         recordList.length,
-    //         page,
-    //         pageSize
-    //     );
-    //     const pageOfItems = recordList.slice(
-    //         pager.startIndex,
-    //         pager.endIndex + 1
-    //     );
-    //     res.status(200).send({
-    //         success: true,
-    //         payload: {
-    //         pager: pager,
-    //         data: pageOfItems,
-    //         message: "List of Data returned successfully!!",
-    //         },
-    //     });
-    //     } catch (err) {
-    //     res.status(500).send({
-    //         success: false,
-    //         payload: {
-    //         message: err.message,
-    //         },
-    //     });
-    //     }
-    // }
   async getAll(req, res) {
     try {
       const recordList = await this.model.getAll();
