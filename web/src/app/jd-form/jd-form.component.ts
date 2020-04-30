2ff8e93fdd97329cacfd0357de6f728c36019596
balani98 <balanideepanshu92@gmail.com>
2020-04-30 22:34:47 +0530

    some minor changes in Pdf UI done

--- web/src/app/jd-form/jd-form.component.ts
+++ web/src/app/jd-form/jd-form.component.ts
@@ -3,10 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AppServicesService } from './../services/app-services.service';
 import { ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import {Router}from '@angular/router'
-import * as jsPDF from 'jspdf'
 import{jobDescription}from '../models/jobDescription.interface'
-import html2canvas from 'html2canvas';  
-  
 @Component({
   selector: 'app-jd-form',
   templateUrl: './jd-form.component.html',
@@ -86,43 +83,5 @@ export class JdFormComponent implements OnInit {
     });
     
   } 
-  image = {
-    name: "Image 1", url:"https://d2q79iu7y748jz.cloudfront.net/s/_logo/03b1f41ce2752d969c31c4c10182a005"
-  }
  
-  public convertToPDF()
-  {
-  var data = document.getElementById('content');
-  html2canvas(data).then(canvas => {
-  // Few necessary setting options
-  var imgWidth = 208;
-  var pageHeight = 295;
-  var imgHeight = canvas.height * imgWidth / canvas.width;
-  var heightLeft = imgHeight;
-   
-  const contentDataURL = canvas.toDataURL('image/png')
-  let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
-  var position = 0;
-  pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
-  pdf.save('new-file.pdf'); // Generated PDF
-  });
-  }
-  downloadPdf(){
-
-   let doc=new jsPDF();
-   let specialElementHandlers={
-     '#editor':function(element,renderer){
-           return true;
-       }
-       
-   }
-     let content=this.content.nativeElement
-     doc.fromHTML(content.innerHTML,15,15,{
-      "width":198,
-      "element":specialElementHandlers
-
-     }); 
-     doc.save(this.data.jdId+".pdf");
-  }
-  
 }
--- web/src/app/jd-form/jd-pdf/jd-pdf.component.ts
+++ web/src/app/jd-form/jd-pdf/jd-pdf.component.ts
@@ -46,7 +46,7 @@ export class JdPdfComponent implements OnInit {
   const contentDataURL = canvas.toDataURL('image/png')
   let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
   var position = 0;
-  pdf.addImage(contentDataURL, 'PNG', 0, position, 2*imgWidth, 2*imgHeight)
+  pdf.addImage(contentDataURL, 'PNG', 0, position, 1.5*imgWidth, 1.5*imgHeight)
   // pdf.setTextColor(255,0,0);
   // pdf.setFillColor(135, 124,45,0);
   // pdf.setFontType("italic");
