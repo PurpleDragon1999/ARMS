6015bfb0d9f1608ec1018b85914f3d986a3d4b1d
nangchetnamaw <nangchetnamaw@gmail.com>
2020-05-01 16:43:38 +0530

    jd-modal component added

---
+++ web/src/app/jd-modal/jd-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JdModalComponent } from './jd-modal.component';
+
+describe('JdModalComponent', () => {
+  let component: JdModalComponent;
+  let fixture: ComponentFixture<JdModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JdModalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JdModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ web/src/app/jd-modal/jd-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jd-modal',
+  templateUrl: './jd-modal.component.html',
+  styleUrls: ['./jd-modal.component.scss']
+})
+export class JdModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
