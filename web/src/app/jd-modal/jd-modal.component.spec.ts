5a95d609500cce5348fc9fcc0abfcbe30651c6c7
horaanchal <horaanchal@gmail.com>
2020-05-06 12:11:08 +0530

    Testing issues resolved

--- web/src/app/jd-modal/jd-modal.component.spec.ts
+++ web/src/app/jd-modal/jd-modal.component.spec.ts
@@ -1,3 +1,4 @@
+
 // import { BrowserModule } from '@angular/platform-browser';
 // import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 // import { JdModalComponent } from './jd-modal.component';
@@ -10,7 +11,7 @@
 //   beforeEach(async(() => {
 //     TestBed.configureTestingModule({
 //       declarations: [ JdModalComponent ],
-//       imports: [ FormsModule, ReactiveFormsModule,BrowserModule ]
+//       imports: [ FormsModule, ReactiveFormsModule,BrowserModule], 
   
 //     })
 //     .compileComponents();
