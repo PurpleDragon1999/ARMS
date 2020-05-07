12ddb4bbefeaeafcee14f540712c6f0d3b6ef451
horaanchal <horaanchal@gmail.com>
2020-05-07 20:12:01 +0530

    Testing tried

--- web/src/app/jd-modal/jd-modal.component.spec.ts
+++ web/src/app/jd-modal/jd-modal.component.spec.ts
@@ -1,5 +1,5 @@
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { JdModalComponent } from './jd-modal.component';
@@ -12,7 +12,8 @@ describe('JdModalComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ JdModalComponent ],
-      imports: [ FormsModule, ReactiveFormsModule,BrowserModule,HttpClientModule,RouterModule], 
+      imports: [ FormsModule, ReactiveFormsModule,BrowserModule,  HttpClientTestingModule,
+        RouterTestingModule.withRoutes([])], 
   
     })
     .compileComponents();
