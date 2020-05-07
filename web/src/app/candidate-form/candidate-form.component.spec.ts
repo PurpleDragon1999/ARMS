ad0d5c27625c1e537b0b05ca7f1759c7bcc3abf4
nangchetnamaw <nangchetnamaw@gmail.com>
2020-05-07 19:53:31 +0530

    commit for test errors

--- web/src/app/candidate-form/candidate-form.component.spec.ts
+++ web/src/app/candidate-form/candidate-form.component.spec.ts
@@ -1,25 +1,29 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { CandidateFormComponent } from './candidate-form.component';
+import { FileSelectDirective } from 'ng2-file-upload';
+import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CandidateFormComponent } from './candidate-form.component';
 
-// describe('CandidateFormComponent', () => {
-//   let component: CandidateFormComponent;
-//   let fixture: ComponentFixture<CandidateFormComponent>;
+describe('CandidateFormComponent', () => {
+  let component: CandidateFormComponent;
+  let fixture: ComponentFixture<CandidateFormComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ CandidateFormComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CandidateFormComponent ],
+      imports : [ FileSelectDirective, NgbActiveModal, NgbModule, FormsModule, ReactiveFormsModule]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CandidateFormComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CandidateFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
