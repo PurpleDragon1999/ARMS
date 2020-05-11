dbf4864709f0d6e2485dca1a8b295e6cc9254c94
horaanchal <horaanchal@gmail.com>
2020-05-11 17:33:41 +0530

    validation improvised

--- web/src/app/jd-modal/jd-modal.component.ts
+++ web/src/app/jd-modal/jd-modal.component.ts
@@ -1,146 +1,100 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AppServicesService } from "../services/app-services.service";
 import { ViewChild, ElementRef, AfterViewInit } from "@angular/core";
-import { IResponse } from "src/app/models/response.interface";
 import { Router } from "@angular/router";
 import { ModalComponent } from "../reusable-components/modal/modal.component";
 import * as jsPDF from "jspdf";
 import { jobDescription } from "../models/jobDescription.interface";
 import html2canvas from "html2canvas";
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
 import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
-import { DynamicGrid } from "../grid.model";
 
 @Component({
-  selector: 'app-jd-modal',
-  templateUrl: './jd-modal.component.html',
-  styleUrls: ['./jd-modal.component.scss']
+  selector: "app-jd-modal",
+  templateUrl: "./jd-modal.component.html",
+  styleUrls: ["./jd-modal.component.scss"],
 })
 export class JdModalComponent implements OnInit {
-
   @Input()
-  jdUpdateId : string
+  jdUpdateId: string;
 
-  constructor( private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private _service: AppServicesService,
     private router: Router,
-    private modalService:NgbModal,
-    private _router:Router) {}
+    private modalService: NgbModal,
+    private _router: Router
+  ) {}
 
-    dynamicArray: Array<DynamicGrid> = [];
-    newDynamic: any = {};
+  @Output()
+  closeModal: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    @Output()
-    closeModal: EventEmitter<boolean> = new EventEmitter<boolean>();
-  
-    @ViewChild("jdId", { static: false }) jdId: ElementRef;
-    @ViewChild("jdTitle", { static: false }) jdTitle: ElementRef;
-    @ViewChild("openingDate", { static: false }) openingDate: ElementRef;
-    @ViewChild("closingDate", { static: false }) closingDate: ElementRef;
-    @ViewChild("jobProfileDescription", { static: false }) jobProfileDescription: ElementRef;
-    @ViewChild("skills", { static: false }) skills: ElementRef;
-    @ViewChild("jobType", { static: false }) jobType: ElementRef;
-    @ViewChild("eligibilityCriteria", { static: false })
-    eligibilityCriteria: ElementRef;
-    @ViewChild("location", { static: false }) location: ElementRef;
-    @ViewChild("salary", { static: false }) salary: ElementRef;
-    @ViewChild("vacancies", { static: false }) vacancies: ElementRef;
-    @ViewChild("content", { static: true }) content: ElementRef;
-    
-    
-    jobArray:any;
-    eligibilityCriteriaOptions: String;
-    locationOptions: String;
-    jobTypeOptions: String;
-    jobListingForm: FormGroup;
-    submitted = false;
-    jdFormObject: jobDescription;
-    data: jobDescription;
-   
+  @ViewChild("jdId", { static: false }) jdId: ElementRef;
+  @ViewChild("jdTitle", { static: false }) jdTitle: ElementRef;
+  @ViewChild("openingDate", { static: false }) openingDate: ElementRef;
+  @ViewChild("closingDate", { static: false }) closingDate: ElementRef;
+  @ViewChild("jobProfileDescription", { static: false })
+  jobProfileDescription: ElementRef;
+  @ViewChild("skills", { static: false }) skills: ElementRef;
+  @ViewChild("jobType", { static: false }) jobType: ElementRef;
+  @ViewChild("eligibilityCriteria", { static: false })
+  eligibilityCriteria: ElementRef;
+  @ViewChild("location", { static: false }) location: ElementRef;
+  @ViewChild("salary", { static: false }) salary: ElementRef;
+  @ViewChild("vacancies", { static: false }) vacancies: ElementRef;
+  @ViewChild("content", { static: true }) content: ElementRef;
 
-    selectChangeHandlerEligibilityCriteria(event: any) {
-      this.eligibilityCriteriaOptions = event.target.value;
-    }
-  
-    selectChangeHandlerLocation(event: any) {
-      this.locationOptions = event.target.value;
-    }
-  
-    selectChangeHandlerJobType(event: any) {
-      this.jobTypeOptions = event.target.value;
-    }
-    ngOnInit() {
-      this.loadJobData(this.jdUpdateId);
-      this.jobListingForm = this.formBuilder.group({
-        jobId: ["", Validators.required],
-        jobTitle: ["", Validators.required],
-        openingDate: ["", Validators.required],
-        closingDate: ["", Validators.required],
-        jobDescription: ["", Validators.required],
-        skills: ["", Validators.required],
-        jobType: ["", Validators.required],
-        eligibilityCriteria: ["", Validators.required],
-        location: ["", Validators.required],
-        salary: ["", Validators.required],
-        vacancies: ["", Validators.required],
-      });
-    }
-    get f() {
-      return this.jobListingForm.controls;
-    }
-    
-    error: any = { isError: false, errorMessage: "" };
-  
-    onSubmit() {
-      this.submitted = true;
-      // stop here if form is invalid
-      if (this.jobListingForm.invalid) {
-        return;
+  jobArray: any;
+  eligibilityCriteriaOptions: String;
+  locationOptions: String;
+  jobTypeOptions: String;
+  jobListingForm: FormGroup;
+  submitted = false;
+  jdFormObject: jobDescription;
+  data: jobDescription;
+
+  ngOnInit() {
+    this.loadJobData(this.jdUpdateId);
+  }
+
+  loadJobData(Id: string) {
+    this._service.getJobsById(Id).subscribe((res: any) => {
+      if (res.success) {
+        this.jobArray = res.payload.data;
+        this.setJobData();
       }
-      alert("SUCCESS!! :-)\n\n" + JSON.stringify(this.jobListingForm.value));
-    }
-    
-   
+    });
+  }
+  setJobData() {
+    this.jdId = this.jobArray.jdId;
+    this.jdTitle = this.jobArray.jdTitle;
+    this.openingDate = this.jobArray.openingDate.slice(0, 10);
+    this.closingDate = this.jobArray.closingDate.slice(0, 10);
+    this.jobProfileDescription = this.jobArray.jobProfileDescription;
+    this.skills = this.jobArray.skills;
+    this.jobType = this.jobArray.jobType;
+    this.eligibilityCriteria = this.jobArray.eligibilityCriteria;
+    this.location = this.jobArray.location;
+    this.salary = this.jobArray.salary;
+    this.vacancies = this.jobArray.vacancies;
+  }
 
-    loadJobData(Id:string){
-      this._service.getJobsById(Id).subscribe((res:any) =>{
-        if(res.success){
-          this.jobArray= res.payload.data;
-          this.setJobData();
-        }
-      });
-    }
-    setJobData(){
-      this.jdId = this.jobArray.jdId;
-      this.jdTitle = this.jobArray.jdTitle;
-      this.openingDate = this.jobArray.openingDate.slice(0,10);
-      this.closingDate = this.jobArray.closingDate.slice(0,10);
-      this.jobProfileDescription = this.jobArray.jobProfileDescription;
-      this.skills = this.jobArray.skills;
-      this.jobType = this.jobArray.jobType;
-      this.eligibilityCriteria = this.jobArray.eligibilityCriteria;
-      this.location = this.jobArray.location;
-      this.salary = this.jobArray.salary;
-      this.vacancies = this.jobArray.vacancies;
-    };
-    
-
-    sendUpdateRequest(jdFormObject: any ){
-      console.log(jdFormObject)
-        this._service.updateJobInfo(jdFormObject,this.jobArray._id).subscribe((res: any) => {
-        console.log(res, 'response');
-        const modalRef= this.modalService.open(ModalComponent);
+  sendUpdateRequest(jdFormObject: any) {
+    console.log(jdFormObject);
+    this._service.updateJobInfo(jdFormObject, this.jobArray._id).subscribe(
+      (res: any) => {
+        console.log(res, "response");
+        const modalRef = this.modalService.open(ModalComponent);
         modalRef.componentInstance.shouldConfirm = false;
         modalRef.componentInstance.success = res.success;
         modalRef.componentInstance.message = res.payload.message;
         modalRef.componentInstance.closeModal.subscribe((rerender: boolean) => {
           modalRef.close();
         });
-      
       },
       (error: HttpErrorResponse) => {
-        console.log(error, 'response');
+        console.log(error, "response");
         const modalRef: NgbModalRef = this.modalService.open(ModalComponent);
         modalRef.componentInstance.shouldConfirm = false;
         modalRef.componentInstance.success = error.error.success;
@@ -151,48 +105,26 @@ export class JdModalComponent implements OnInit {
         this.data = error.error.payload.data;
         this.router.navigate(["/jd-pdf", this.data.jdId]);
       }
-      ); 
-    }
-  
-    jdFormData() {
-      this.jdFormObject = {
-        jdId: `CYGJID${this.jdId.nativeElement.value}`,
-        jdTitle: this.jdTitle.nativeElement.value,
-        openingDate: this.openingDate.nativeElement.value,
-        closingDate: this.closingDate.nativeElement.value,
-        jobProfileDescription: this.jobProfileDescription.nativeElement.value,
-        skills: this.skills.nativeElement.value,
-        jobType: this.jobType.nativeElement.value,
-        eligibilityCriteria: this.eligibilityCriteria.nativeElement.value,
-        location: this.location.nativeElement.value,
-        salary: this.salary.nativeElement.value,
-        vacancies: this.vacancies.nativeElement.value,
-      };
-      if (
-        new Date(this.jobListingForm.controls["closingDate"].value) <
-        new Date(this.jobListingForm.controls["openingDate"].value)
-      ) {
-        this.error = {
-          isError: true,
-          errorMessage: "Closing Date cannot be before Opening date",
-        };
-        return;
-      }
-      
-  
+    );
   }
-  deleteRow(index) {
-    if (this.dynamicArray.length == 1) {
-      return false;
-    } else {
-      this.dynamicArray.splice(index, 1);
-      return true;
-    }
+
+  jdFormData() {
+    this.jdFormObject = {
+      jdId: `CYGJID${this.jdId.nativeElement.value}`,
+      jdTitle: this.jdTitle.nativeElement.value,
+      openingDate: this.openingDate.nativeElement.value,
+      closingDate: this.closingDate.nativeElement.value,
+      jobProfileDescription: this.jobProfileDescription.nativeElement.value,
+      skills: this.skills.nativeElement.value,
+      jobType: this.jobType.nativeElement.value,
+      eligibilityCriteria: this.eligibilityCriteria.nativeElement.value,
+      location: this.location.nativeElement.value,
+      salary: this.salary.nativeElement.value,
+      vacancies: this.vacancies.nativeElement.value,
+    };
   }
-  modalClose(rerender: boolean){
+
+  modalClose(rerender: boolean) {
     this.closeModal.emit(rerender);
   }
- 
-  
-  
 }
