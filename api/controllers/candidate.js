bf25afd4c67af085511668fa644dfb7f59109bfd
Himanshu Sharma <hsharma151197@gmail.com>
2020-05-15 01:20:52 +0530

    Added Candidate Search and Pagination

--- api/controllers/candidate.js
+++ api/controllers/candidate.js
@@ -14,9 +14,9 @@ async function validateCandidate(candidate) {
     throw new Error("This Aadhar number is already registered")
     return
   }
-  if(validator.isvalid(candidate.aadhar)){
-      throw new Error("This aadhar number is not valid");
-      return
+  if (validator.isvalid(candidate.aadhar)) {
+    throw new Error("This aadhar number is not valid");
+    return
   }
 
   if (!candidate.cv) {
@@ -113,17 +113,26 @@ class Candidate extends Base {
     }
   }
 
-  async getAll(req, res) {
+  async searchRecord(req, res) {
     try {
-      let candidatesList = await candidateModel.getAll()
-      candidatesList = await Promise.all(candidatesList.map(async (candidate) => {
-        return { ...candidate.toObject(), cv: fs.readFileSync(candidate.cv) }
-      }))
-      req.body.records = candidatesList
+      let queryObject = {
+        $regex: ".*^" + req.query.character + ".*",
+        $options: "i",
+      };
+
+      let candidateList = await candidateModel.getAll({ $or: [{ name: queryObject }] });
+
+      candidateList = await Promise.all(candidateList.map(async (candidate) => {
+        if (fs.existsSync(candidate.cv)) {
+          return { ...candidate.toObject(), cv: fs.readFileSync(candidate.cv) };
+        }
+        return { ...candidate.toObject() };
+      }));
+
+      req.body.records = candidateList;
       super.getPaginatedResult(req, res)
     }
     catch (err) {
-      console.log(err)
       res.send({
         success: false,
         payload: {
