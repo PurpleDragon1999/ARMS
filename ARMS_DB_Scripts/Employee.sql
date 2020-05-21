e77eba21cbc3453b19ca03032a7943b07ee58525
ShivaniBansal1 <51862088+ShivaniBansal1@users.noreply.github.com>
2020-05-21 16:10:22 +0530

    changed casing, added createdAt and modifiedAt with update trigger

--- ARMS_DB_Scripts/Employee.sql
+++ ARMS_DB_Scripts/Employee.sql
@@ -1,13 +1,24 @@
 create table Employee (
-EmployeeNo int identity(1000,1),
-EmployeeId  as 'CYG' + cast(EmployeeNo as varchar(100)) persisted primary key,
+createdAt datetime2(3) default (sysdatetime()),
+modifiedAt datetime2(3) default (sysdatetime()),
 
-Name varchar(300) not null,
-Designation varchar(300) check (Designation in ('Intern', 'Consultant 1', 'Consultant 2', 'Associate 1',
+employeeNo int identity(1000,1),
+id  as 'CYG' + cast(employeeNo as varchar(100)) persisted primary key,
+
+[name] varchar(300) not null,
+designation varchar(300) check (Designation in ('Intern', 'Consultant 1', 'Consultant 2', 'Associate 1',
 'Associate 2')) default 'Consultant 1' not null,
 
-Role varchar(255) check (Role in ('SuperUser', 'Admin', 'Employee')) default 'Employee',
-Email varchar(400) not null unique ,
-ProfileImage varchar(8000),
+[role] varchar(255) check (Role in ('SuperUser', 'Admin', 'Employee')) default 'Employee',
+email varchar(400) not null unique ,
+profileImage varchar(255),
 
 )
+
+CREATE TRIGGER trg_UpdateEmployee
+ON Employee
+AFTER UPDATE
+AS
+    UPDATE Employee
+    SET modifiedAt  = sysdatetime()
+    WHERE id IN (SELECT DISTINCT id FROM Inserted)
