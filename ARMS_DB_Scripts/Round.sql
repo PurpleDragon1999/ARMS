17a202f6daf187f2be363d5120db46a782c85099
ShivaniBansal1 <51862088+ShivaniBansal1@users.noreply.github.com>
2020-05-21 16:12:52 +0530

    changed casing, added createdAt and modifiedAt with update trigger and other suggested changes

--- ARMS_DB_Scripts/Round.sql
+++ ARMS_DB_Scripts/Round.sql
@@ -1,18 +1,25 @@
 create table Round(
-Round_Number int identity(1000,1) ,
-Round_Id as 'CYGRID' + cast(Round_Number as varchar(200)) persisted primary key,
+createdAt datetime2(3) default (sysdatetime()),
+modifiedAt datetime2(3) default (sysdatetime()),
+roundNo int identity(1000,1) ,
+id as 'CYGRID' + cast(roundNo as varchar(200)) persisted primary key,
 
-Interview_Id varchar(106) not null,
-constraint FK_Round_Interview foreign key (Interview_Id) references Interview(Interview_Id) ,
+interviewId varchar(106) not null,
+constraint FK_Round_Interview foreign key (interviewId) references Interview(id) ,
 
-RoundType varchar(200) not null,
-[Time] time not null,
+roundType varchar(200) not null,
+[time] time not null,
 
-RoundNumber int not null,
+roundNumber int not null,
 
 )
 
-insert into Round (Interview_Id, RoundType,[Time], RoundNumber)
-values ('CYGIID1000','Written','10:15', 6)
+CREATE TRIGGER trg_UpdateRound
+ON Round
+AFTER UPDATE
+AS
+    UPDATE Round
+    SET modifiedAt  = sysdatetime()
+    WHERE id IN (SELECT DISTINCT id FROM Inserted)
 
-select * from Round
+
