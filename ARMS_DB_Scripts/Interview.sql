dfa62e1ec91a6ed824a35866e2a138ed93f824b1
ShivaniBansal1 <51862088+ShivaniBansal1@users.noreply.github.com>
2020-05-21 16:11:26 +0530

    changed casing, added createdAt and modifiedAt with update trigger and other suggested changes

--- ARMS_DB_Scripts/Interview.sql
+++ ARMS_DB_Scripts/Interview.sql
@@ -1,11 +1,21 @@
 create table Interview (
-Interview_Number int identity(1000,1),
-Interview_Id as 'CYGIID' + cast(Interview_Number as varchar(100)) persisted primary key,
-Date date not null,
-Time time not null,
-Venue varchar(255) not null,
-NoOfRounds int not null,
-JD_Id varchar(8000) not null,
-constraint FK_Interview foreign key (JD_Id) references JobDescription(JD_Id),
+createdAt datetime2(3) default (sysdatetime()),
+modifiedAt datetime2(3) default (sysdatetime()),
+interviewNumber int identity(1000,1),
+id as 'CYGIID' + cast(interviewNumber as varchar(100)) persisted primary key,
+[date] date not null,
+[time] time not null,
+[venue] varchar(255) not null,
+[noOfRounds] int not null,
+jobDescriptionId varchar(106) not null,
+constraint FK_Interview foreign key (jobDescriptionId) references JobDescription(id),
 
 )
+
+CREATE TRIGGER trg_UpdateInterview
+ON Interview
+AFTER UPDATE
+AS
+    UPDATE Interview
+    SET modifiedAt  = sysdatetime()
+    WHERE id IN (SELECT DISTINCT id FROM Inserted)
