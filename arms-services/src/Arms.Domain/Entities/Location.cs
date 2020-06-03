9b1665667353f8bab248cdf19c59a6b3a4f81cfb
balani98 <balanideepanshu92@gmail.com>
2020-06-03 12:34:25 +0530

    conflicts resolved in Location.cs

--- arms-services/src/Arms.Domain/Entities/Location.cs
+++ arms-services/src/Arms.Domain/Entities/Location.cs
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-﻿namespace Arms.Domain.Entities
-{
-    public class Location
-=======
-using Hrms.Core.Domains.Entities;
-using System;
-
 namespace Arms.Domain.Entities
 {
-    public partial class Location
->>>>>>> 745dd668a5697c890f6fbba5f78a466656a41b76
+    public class Location
     {
-        public int Id { get; set; }
-        public string Code { get; set; }
-        public string LocationName { get; set; }
-        public int JobId { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public string CreatedBy { get; set; }
-        public DateTime ModifiedAt { get; set; }
-        public string ModifiedBy { get; set; }
-
-        public JobDescription Job { get; set; }
+        public int LocationId { get; set; }
+        public string Name { get; set; }
     }
 }
