613fe0bce981ac329ee54287aff758ac286b62d5
abharana24 <59593981+abharana24@users.noreply.github.com>
2020-06-08 22:37:01 +0530

    commented code removed, naming changed

--- web/src/app/settings/settings.component.ts
+++ web/src/app/settings/settings.component.ts
@@ -1,56 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup,FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.scss"],
 })
 export class SettingsComponent implements OnInit {
+  ngOnInit() {}
 
-  ngOnInit() { }
-
-  roundTypeForm:FormGroup;
-  addRound:Boolean=false;
+  roundTypeForm: FormGroup;
+  addRound: Boolean = false;
 
   constructor(private fb: FormBuilder) {
-
     this.roundTypeForm = this.fb.group({
-      roundTypes: this.fb.array([]) ,
+      roundTypes: this.fb.array([]),
     });
   }
 
   roundTypes(): FormArray {
-    return this.roundTypeForm.get("roundTypes") as FormArray
+    return this.roundTypeForm.get("roundTypes") as FormArray;
   }
 
   newRoundType(): FormGroup {
     return this.fb.group({
-      firstName: '',
-      criteria: this.fb.array([])
+      firstName: "",
+      criteria: this.fb.array([]),
     });
   }
 
   addRoundType() {
     console.log("Adding a round type");
-    this.addRound=true;
+    this.addRound = true;
     this.roundTypes().push(this.newRoundType());
   }
 
   removeRoundType(typeIndex: number) {
     this.roundTypes().removeAt(typeIndex);
-    if(typeIndex==0){
-      this.addRound=false;
+    if (typeIndex == 0) {
+      this.addRound = false;
     }
   }
 
   roundCriteria(typeIndex: number): FormArray {
-    return this.roundTypes().at(typeIndex).get("criteria") as FormArray
+    return this.roundTypes().at(typeIndex).get("criteria") as FormArray;
   }
 
   newCriterion(): FormGroup {
     return this.fb.group({
-      criterion: ''
+      criterion: "",
     });
   }
 
@@ -65,5 +69,4 @@ export class SettingsComponent implements OnInit {
   onSubmit() {
     console.log(this.roundTypeForm.value);
   }
-
 }
