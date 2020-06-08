3669d0408eaf85f2123da2d4be122ca3b1116082
abharana24 <59593981+abharana24@users.noreply.github.com>
2020-06-08 22:27:32 +0530

    minor changes

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule,  } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MsalInterceptor, MsalModule } from '@azure/msal-angular';
 import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
--- web/src/app/settings/settings.component.ts
+++ web/src/app/settings/settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup,FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
