9f09a50856335a89b464624e1b5b9e1d19b2dfca
Shivani Bansal <bansalshivani703@gmail.com>
2020-06-12 15:40:54 +0530

    removed commented code

--- web/src/app/candidate-form/candidate-form.component.ts
+++ web/src/app/candidate-form/candidate-form.component.ts
@@ -98,29 +98,6 @@ export class CandidateFormComponent implements OnInit {
   jdObjectId : String
   formData:FormData = new FormData();
 
-  fileChange(files) {
-    
-    let fileList: FileList = files;
-    if(fileList.length > 0) {
-        let file: File = fileList[0];
-        let formData:FormData = new FormData();
-        formData.append(file.name, file);
-        
-        // let headers = new Headers();
-        // /** In Angular 5, including the header Content-Type can invalidate your request */
-        // headers.append('Content-Type', 'multipart/form-data');
-        // headers.append('Accept', 'application/json');
-        // let options = new RequestOptions({ headers: headers });
-        // this.http.post(`${this.apiEndPoint}`, formData, options)
-        //     .map(res => res.json())
-        //     .catch(error => Observable.throw(error))
-        //     .subscribe(
-        //         data => console.log('success'),
-        //         error => console.log(error)
-        //     )
-    }
-}
-
 validateApplication(applicationObj : ICandidate){
   if (applicationObj.nationality == "Indian"){
     
