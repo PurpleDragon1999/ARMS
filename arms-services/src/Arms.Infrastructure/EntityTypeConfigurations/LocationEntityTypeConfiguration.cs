d9431225339cb06400c8dd65de82eff4ffa89a69
horaanchal <horaanchal@gmail.com>
2020-06-12 11:50:01 +0530

    minor changes

--- arms-services/src/Arms.Infrastructure/EntityTypeConfigurations/LocationEntityTypeConfiguration.cs
+++ arms-services/src/Arms.Infrastructure/EntityTypeConfigurations/LocationEntityTypeConfiguration.cs
@@ -12,12 +12,41 @@ namespace Arms.Infrastructure.EntityTypeConfigurations
         public void Configure(EntityTypeBuilder<Location> builder)
         {
             builder.ToTable("Location", "ARMS");
+            builder.Property(e => e.id).HasColumnName("id");
 
-            builder.Property(e => e.LocationId).HasColumnName("LocationId");
+            //builder.Property(e => e.id).ValueGeneratedOnAdd();
+            builder.Property(e => e.code)
+                .IsRequired()
+                .HasColumnName("code")
+                .HasMaxLength(57)
+                .IsUnicode(false)
+                .HasComputedColumnSql("('CYGLCID'+CONVERT([varchar](50),[id]))");
 
-            builder.Property(e => e.LocationName)
-           .IsRequired()
-           .HasColumnName("LocationName");
+            builder.Property(e => e.createdAt)
+                .HasColumnName("createdAt")
+                .HasDefaultValueSql("(sysdatetime())");
+
+            builder.Property(e => e.createdBy)
+                .IsRequired()
+                .HasColumnName("createdBy")
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            builder.Property(e => e.locationName)
+                .IsRequired()
+                .HasColumnName("locationName")
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            builder.Property(e => e.modifiedAt)
+                .HasColumnName("modifiedAt")
+                .HasDefaultValueSql("(sysdatetime())");
+
+            builder.Property(e => e.modifiedBy)
+                .IsRequired()
+                .HasColumnName("modifiedBy")
+                .HasMaxLength(255)
+                .IsUnicode(false);
         }
     }
 }
