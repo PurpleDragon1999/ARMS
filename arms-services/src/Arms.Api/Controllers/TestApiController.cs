6543c49556d8cf3a1c0043cb2fea9a94f2f0b9bf
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-06-22 01:55:38 +0530

    test api controller

---
+++ arms-services/src/Arms.Api/Controllers/TestApiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Arms.Application.Services.Users;
+using Arms.Domain.Entities;
+using Arms.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arms.Api.Controllers
+{
+    public class TestApiController
+    {
+        ArmsDbContext _context;
+        public TestApiController(ArmsDbContext armsContext)
+        {
+            _context = armsContext;
+        }
+
+        [HttpGet]
+        [Route("Data")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public string Index()
+        {
+            return "This is a test route.";
+        }
+    }
+}
